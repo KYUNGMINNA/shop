11f4751e55229a9cdcb6af3bf07af7b7e1cc4f9d
KYUNGMINNA <nkm4095@naver.com>
2022-09-09 17:47:53 +0900

    update

--- src/main/java/com/shop/sale/controller/AccountController.java
+++ src/main/java/com/shop/sale/controller/AccountController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.shop.sale.account.service.AccountService;
 import com.shop.sale.account.service.IAccountService;
 import com.shop.sale.command.AccountVO;
+
 
 @Controller
 @RequestMapping("/account")
@@ -20,15 +23,38 @@ public class AccountController {
 	private IAccountService accountService;
 
 
+	
 	//회원가입
-	@GetMapping("/login")
+	@GetMapping("/signIn")
 	public String signUp() {
 
 		return "account/account_sing_up";
 	}
+	
+	
+	//아이디 중복 체크 비동기 통신
+	@PostMapping("/duplication")
+	@ResponseBody
+	public String idDuplication(@RequestBody String user_id) {
+		System.out.println("컨트롤러 호출됨");
+		
+		System.out.println(accountService.accountDuplicationId(user_id));
+		if(accountService.accountDuplicationId(user_id)==1) {
+			System.out.println("중복된 아이디가 존재 함 ");
+			return "duplication";
+		}
+	
+		return "not_duplication";
+		
+		
+	}
+	
+	
+	
+	
 
 	//회원 가입  결과 
-	@PostMapping("/login")
+	@PostMapping("/signIn")
 	public String signUp(HttpSession session,AccountVO accountVO) {
 		accountVO.setAccount_type("개인");
 
@@ -37,11 +63,7 @@ public class AccountController {
 			System.out.println("세션에 저장 되었습니다.");
 			System.out.println("방금 들어간 데이터의 기본키 값은 : "+accountVO.getAccount_pk());
 		}
-		
-		
-		
-
-		return "forward:/board";
+		return "redirect:/";
 	}
 
 
