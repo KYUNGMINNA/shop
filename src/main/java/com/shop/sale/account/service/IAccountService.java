0b4f3783a4b0fb40e48c7480c04ead08d6e468cf
KYUNGMINNA <nkm4095@naver.com>
2022-09-16 14:28:03 +0900

    update

--- src/main/java/com/shop/sale/account/service/IAccountService.java
+++ src/main/java/com/shop/sale/account/service/IAccountService.java
@@ -5,4 +5,7 @@ import com.shop.sale.command.AccountVO;
 public interface IAccountService {
 
 	int accountInsert(AccountVO accountVO);
+	
+	//아아디 중복체크
+	int accountDuplicationId(String account_id);
 }
