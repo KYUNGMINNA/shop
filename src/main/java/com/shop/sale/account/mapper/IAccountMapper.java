e0e00ed10b19df1186e57c828cc394d1fa0ccb7b
KYUNGMINNA <nkm4095@naver.com>
2022-09-16 14:27:17 +0900

    update

--- src/main/java/com/shop/sale/account/mapper/IAccountMapper.java
+++ src/main/java/com/shop/sale/account/mapper/IAccountMapper.java
@@ -4,5 +4,9 @@ import com.shop.sale.command.AccountVO;
 
 public interface IAccountMapper {
 
+	//회원 가입
 	int accountInsert(AccountVO accountVO);
+	
+	//아아디 중복체크
+	int accountDuplicationId(String account_id);
 }
