ae2e476121c188f34fda0703bbac2ee85657dac0
KYUNGMIN <nkm4095@naver.com>
2022-11-20 19:54:12 +0900

    Service Interface

--- src/main/java/com/shop/sale/account/service/IAccountService.java
+++ src/main/java/com/shop/sale/account/service/IAccountService.java
@@ -1,9 +1,15 @@
 package com.shop.sale.account.service;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.shop.sale.command.AccountVO;
 
 public interface IAccountService {
 
+	//로그인
+	AccountVO login(String accountId);
+		
+	
 	int accountInsert(AccountVO accountVO);
 	
 	//아아디 중복체크
