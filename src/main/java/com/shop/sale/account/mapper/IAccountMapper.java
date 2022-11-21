89a94e72d0f7786757e020e91fbcd418481139c1
KYUNGMIN <nkm4095@naver.com>
2022-11-21 22:46:31 +0900

    account mapper

--- src/main/java/com/shop/sale/account/mapper/IAccountMapper.java
+++ src/main/java/com/shop/sale/account/mapper/IAccountMapper.java
@@ -1,9 +1,15 @@
 package com.shop.sale.account.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.shop.sale.command.AccountVO;
 
 public interface IAccountMapper {
 
+	//로그인
+	AccountVO login(String accountId);
+	
+	
 	//회원 가입
 	int accountInsert(AccountVO accountVO);
 	
